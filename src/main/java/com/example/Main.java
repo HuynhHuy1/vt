55489fdd6dc098b01a1f0c986801ca071e88c48a
HuyIT <huyhn2.21it@vku.udn.vn>
2024-01-05 01:12:38 +0700

    update

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,22 +1,28 @@
 package com.example;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Manager manager = new Manager();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        Metaria meat1 = new Meat("M1", "Meat 1", LocalDate.parse("22-04-2023",formatter), 20, 11.9);
-        Metaria meat2 = new Meat("M2", "Meat 2", LocalDate.parse("22-05-2023",formatter), 21, 11.6);
-        Metaria meat3 = new Meat("M3", "Meat 3", LocalDate.parse("22-06-2023",formatter), 20, 11.8);
-        Metaria meat4 = new Meat("M4", "Meat 4", LocalDate.parse("22-07-2023",formatter), 22, 11.1);
-        Metaria meat5 = new Meat("M5", "Meat 5", LocalDate.parse("22-08-2023",formatter), 23, 11.5);
-        Metaria crispyFlour1 = new CrispyFlour("M1", "CrispyFlour 1", LocalDate.parse("22-04-2023",formatter), 20, 11);
-        Metaria crispyFlour2 = new CrispyFlour("M2", "CrispyFlour 2", LocalDate.parse("22-05-2023",formatter), 21, 12);
-        Metaria crispyFlour3 = new CrispyFlour("M3", "CrispyFlour 3", LocalDate.parse("22-06-2023",formatter), 20, 13);
-        Metaria crispyFlour4 = new CrispyFlour("M4", "CrispyFlour 4", LocalDate.parse("22-07-2023",formatter), 22, 14);
-        Metaria crispyFlour5 = new CrispyFlour("M5", "CrispyFlour 5", LocalDate.parse("22-08-2023",formatter), 23, 15);
+        Metaria meat1 = new Meat("M1", "Meat1", LocalDate.parse("22-04-2023", formatter), 20, 11.9);
+        Metaria meat2 = new Meat("M2", "Meat2", LocalDate.parse("22-05-2023", formatter), 21, 11.6);
+        Metaria meat3 = new Meat("M3", "Meat3", LocalDate.parse("22-06-2023", formatter), 20, 11.8);
+        Metaria meat4 = new Meat("M4", "Meat4", LocalDate.parse("22-07-2023", formatter), 22, 11.1);
+        Metaria meat5 = new Meat("M5", "Meat5", LocalDate.parse("22-08-2023", formatter), 23, 11.5);
+        Metaria crispyFlour1 = new CrispyFlour("CF1", "CrispyFlour1", LocalDate.parse("22-04-2023", formatter), 20,
+                11);
+        Metaria crispyFlour2 = new CrispyFlour("CF2", "CrispyFlour2", LocalDate.parse("22-05-2023", formatter), 21,
+                12);
+        Metaria crispyFlour3 = new CrispyFlour("CF3", "CrispyFlour3", LocalDate.parse("22-06-2023", formatter), 20,
+                13);
+        Metaria crispyFlour4 = new CrispyFlour("CF4", "CrispyFlour4", LocalDate.parse("22-07-2023", formatter), 22,
+                14);
+        Metaria crispyFlour5 = new CrispyFlour("CF5", "CrispyFlour5", LocalDate.parse("22-08-2023", formatter), 23,
+                15);
         manager.add(meat1);
         manager.add(meat2);
         manager.add(meat3);
@@ -28,9 +34,22 @@ public class Main {
         manager.add(crispyFlour4);
         manager.add(crispyFlour5);
 
+        System.out.println(" Total of 10 Metaria " + manager.totalAmount() + " VND");
 
-        System.out.println(" Total of 10 Metaria "  + manager.totalAmount() + " VND");
+        System.out.println(" Difference between discounted and undiscounted  "
+                + (manager.totalAmount() - manager.totalAmountDiscount()) + " VND");
+
+        try {
+            manager.writeFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         
-        System.out.println(" Difference between discounted and undiscounted  "  + ( manager.totalAmount() - manager.totalAmountDiscount() ) + " VND");
+        
+        manager.readFile();
+        manager.add(new Meat("M6", "Meat1", LocalDate.parse("22-04-2023", formatter), 20, 11.9));
+        manager.remove(new Meat("M1", "Meat1", LocalDate.parse("22-04-2023", formatter), 20, 11.9));
+        manager.update("M4",new Meat("M4", "Meat100", LocalDate.parse("22-04-2023", formatter), 20, 11.9));
     }
+
 }
--- src/main/java/com/example/Manager.java
+++ src/main/java/com/example/Manager.java
@@ -1,37 +1,106 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Manager {
     private List<Metaria> listMetarial;
 
-    public Manager(){
+    public Manager() {
         this.listMetarial = new ArrayList<>();
     }
-    public void add(Metaria metaria){
+
+    public void add(Metaria metaria) throws IOException {
         this.listMetarial.add(metaria);
-    }
-    public void remove(Metaria metaria){
-        this.listMetarial.remove(metaria);
+        this.writeFile();
     }
 
-    public List<Metaria> getList(){
+    public void remove(Metaria metaria) throws IOException {
+        this.listMetarial.remove(metaria);
+        this.writeFile();
+    }
+
+    public void update(String id, Metaria newMetaria) throws IOException {
+        for (Metaria metaria : listMetarial) {
+            if (metaria.getId().equals(id)) {
+                metaria.setName(newMetaria.getName());
+                metaria.setCost(newMetaria.getCost());
+                this.writeFile();
+            }
+        }
+    }
+
+    public List<Metaria> getList() {
         return this.listMetarial;
     }
 
-    public double totalAmount(){
+    public double totalAmount() {
         double total = 0;
         for (Metaria metaria : listMetarial) {
             total += metaria.getAmount();
         }
         return total;
     }
-    public double totalAmountDiscount(){
+
+    public double totalAmountDiscount() {
         double total = 0;
         for (Metaria metaria : listMetarial) {
             total += metaria.getRealMoney();
         }
         return total;
+    }
+
+    public void writeFile() throws IOException {
+        String path = "/Users/mac/Documents/Document Tailwindcss/demo/src/main/java/com/example/write_file.txt";
+        BufferedWriter bw = new BufferedWriter(new FileWriter(path));
+        for (Metaria metaria : listMetarial) {
+            bw.write(metaria.getId() + " " + metaria.getName() + " " + metaria.getManufacturingDate() + " "
+                    + metaria.getAmount() + " " + metaria.getCost() + "\n");
+            bw.flush();
+        }
+    }
+
+    public void readFile() throws IOException {
+        String path = "/Users/mac/Documents/Document Tailwindcss/demo/src/main/java/com/example/read_file.txt";
+        String line;
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        while ((line = br.readLine()) != null) {
+            String parts[] = line.split(" ");
+            if (parts.length == 6) {
+                String id = parts[0];
+                String name = parts[1];
+                String dateString = parts[2];
+                int cost = Integer.parseInt(parts[3]);
+                Double anotherAmount = Double.parseDouble(parts[4]);
+
+                // Chuyển đổi chuỗi ngày thành đối tượng Date
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+                if (id.contains("M")) {
+                    Metaria metaria = new Meat(id, name, LocalDate.parse(dateString, formatter), cost, anotherAmount);
+                    this.listMetarial.add(metaria);
+                } else {
+                    Metaria metaria = new CrispyFlour(id, name, LocalDate.parse(dateString, formatter), cost,
+                            Integer.parseInt("anotherAmount"));
+                    this.listMetarial.add(metaria);
+                }
+            }
+        }
+
+        System.out.println(" Content file reader: ");
+        for (Metaria metaria : listMetarial) {
+            System.out.println(metaria.getId() + " " + metaria.getName() + " " + metaria.getManufacturingDate() + " "
+                    + metaria.getAmount() + " " + metaria.getCost());
+        }
+
     }
 }
